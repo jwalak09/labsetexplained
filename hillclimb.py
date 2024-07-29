8c84027efa5ec0c0f389e0b0bc820d8c829b7bba
jwalak09 <133895297+jwalak09@users.noreply.github.com>
2024-07-29 17:01:26 +0530

    Add files via upload

--- hillclimb.py
+++ hillclimb.py
@@ -1,23 +1,65 @@
-#hill climb
+# Hill Climbing Algorithm
+
 import random
-def fitness(sol):  #to find the sum of solution of the alg
+
+
+def fitness(sol):
+    """
+    Fitness function to evaluate the quality of a solution.
+    In this case, the fitness is simply the sum of the solution vector.
+    """
     return sum(sol)
-def solution(num): #function to find the best soln
-    current_soln = [random.randint(0,1) for _ in range(0,num)]
+
+
+def solution(num):
+    """
+    Hill Climbing algorithm to find the best solution.
+
+    Parameters:
+    num (int): The length of the solution vector.
+
+    Returns:
+    best_soln (list): The best solution found.
+    best_fit (int): The fitness of the best solution.
+    """
+    # Initialize a random solution vector of length 'num'
+    current_soln = [random.randint(0, 1) for _ in range(0, num)]
+
+    # Evaluate the fitness of the initial solution
     current_fit = fitness(current_soln)
+
     while True:
-        index=random.randint(0,num-1) #choosing a random index to start the climbing
-        neighbor=current_soln[:] #copy the curentsoln to neighbor
-        neighbor[index]=1-neighbor[index] #flips the binary value to go to the next neighbor
-        neighbor_fit=fitness(neighbor)#to evaluate the fitness of neighbor we copy neighbor
-        # into this
-        if neighbor_fit>=current_fit:#If the neighbor solution is better (i.e., has higher fitness),
-            # update the current solution and fitness.
-            current_fit=neighbor_fit
-            current_soln=neighbor
+        # Choose a random index to start the climbing
+        index = random.randint(0, num - 1)
+
+        # Create a copy of the current solution to generate a neighbor
+        neighbor = current_soln[:]
+
+        # Flip the binary value at the chosen index to generate a new neighbor
+        neighbor[index] = 1 - neighbor[index]
+
+        # Evaluate the fitness of the neighbor solution
+        neighbor_fit = fitness(neighbor)
+
+        # If the neighbor solution is better (i.e., has higher fitness),
+        # update the current solution and fitness.
+        if neighbor_fit >= current_fit:
+            current_fit = neighbor_fit
+            current_soln = neighbor
         else:
+            # If the neighbor solution is not better, break the loop
             break
-    return current_soln,current_fit
-best_soln,best_fit= solution(3) #specify the length of solution vector can be random
-print('best soln=',best_soln)
-print('best fitnes=',best_fit)
+
+    # Return the best solution and its fitness
+    return current_soln, current_fit
+
+
+# Specify the length of the solution vector (can be random)
+num = 3
+
+# Run the Hill Climbing algorithm
+best_soln, best_fit = solution(num)
+
+# Print the best solution and its fitness
+print('Best solution:', best_soln)
+print('Best fitness:', best_fit)
